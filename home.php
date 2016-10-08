4a37a57b01e46ef97150bc088c7b906852d7a712
evanjfarmer <evanjfarmer@gmail.com>
2016-10-08 01:38:33 -0400

    Changed top bar z-index and changed tpl_blog.php template to home.php

--- home.php
+++ home.php
@@ -1,30 +1,23 @@
-
-/*
-Template Name: Blog Posts
-*/
-
 <?php get_header(); ?>
-
-<?php query_posts('post_type=post&post_status=publish&posts_per_page=10&paged='. get_query_var('paged')); ?>
 			
 	<div id="content">
 	
 		<div id="inner-content" class="row">
 	
 		    <main id="main" class="large-8 medium-8 columns" role="main">
+
+		    	<?php query_posts('post_type=post&post_status=publish&posts_per_page=10&paged='. get_query_var('paged')); ?>
 		    
 			    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			 
 					<!-- To see additional archive styles, visit the /parts directory -->
 					<?php get_template_part( 'parts/loop', 'archive' ); ?>
 				    
-				<?php endwhile; ?>
+				<?php endwhile; ?>	
 
-				<div class="navigation">
-					<span class="newer"><?php previous_posts_link(__('« Newer','example')) ?></span> <span class="older"><?php next_posts_link(__('Older »','example')) ?></span>
-				</div><!-- /.navigation -->	
-
-					<?php joints_page_navi(); ?>
+					<div class="navigation">
+						<span class="newer"><?php previous_posts_link(__('« Newer','example')) ?></span> <span class="older"><?php next_posts_link(__('Older »','example')) ?></span>
+					</div><!-- /.navigation -->
 					
 				<?php else : ?>
 											
@@ -35,8 +28,6 @@ Template Name: Blog Posts
 		    </main> <!-- end #main -->
 		    
 		    <?php get_sidebar(); ?>
-
-		    <?php endif; wp_reset_query(); ?>
 
 		</div> <!-- end #inner-content -->
 
