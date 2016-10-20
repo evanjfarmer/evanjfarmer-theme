0cbe2af76701decd810621745ab2bcc49dc70d62
evanjfarmer <evanjfarmer@gmail.com>
2016-10-20 01:12:58 -0400

    Deleted background image, changed transparency of icon section, changed colors and sizes of icon images, linked front page images to respective pages

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -10,19 +10,19 @@
 			<div class="intro small-12 medium-4 large-4 text-center columns">
 				<a href="/about" class="about hollow button"><h4><strong>About</strong></h4></a>
 				<div class="evanimg">
-				<img src="http://www.evanjfarmer.dev/wp-content/uploads/2016/09/aboutsquare.jpg" alt="Evan About Photo" width="250" height="250" />
+				<a href="/about"><img src="http://www.evanjfarmer.dev/wp-content/uploads/2016/09/aboutsquare.jpg" alt="Evan About Photo" width="250" height="250" /></a>
 				</div><br>
 			</div>
 			<div class="intro small-12 medium-4 large-4 text-center columns">
 				<a href="/projects" class="projects hollow button"><h4><strong>Projects</strong></h4></a>
 				<div class="portfolioimg">
-				<img src="http://www.evanjfarmer.dev/wp-content/uploads/2016/09/portfolioimg.png" alt="Portfolio Icon" width="250" height="250" />
+				<a href="/projects"><img src="http://www.evanjfarmer.dev/wp-content/uploads/2016/09/portfolioimg.png" alt="Portfolio Icon" width="250" height="250" /></a>
 				</div><br>
 			</div>
 			<div class="intro small-12 medium-4 large-4 text-center columns">
 				<a href="/contact" class="go hollow button"><h4><strong>Let's Go!</strong></h4></a>
 				<div class="letsgoimg">
-				<img src="http://www.evanjfarmer.dev/wp-content/uploads/2016/09/letsgoimg.jpg" alt="Man Running Icon" width="250" height="250" />
+				<a href="/contact"><img src="http://www.evanjfarmer.dev/wp-content/uploads/2016/09/letsgoimg.jpg" alt="Man Running Icon" width="250" height="250" /></a>
 				</div><br>
 			</div>
 		</div>
@@ -44,16 +44,16 @@
 			</div>
 			<div class="iconic row">
 				<div class="small-6 large-3 columns">
-				<img class="icon" src="http://www.evanjfarmer.dev/wp-content/uploads/2016/09/img-crop.png" alt="Image Crop Icon" width="200" height="200" />
+				<img class="icon" src="http://www.evanjfarmer.dev/wp-content/uploads/2016/10/img-crop.png" alt="Image Crop Icon" width="120" height="120" />
 				<strong><p>web design & development</p></strong></div>
 				<div class="small-6 large-3 columns">
-				<img class="icon" src="http://www.evanjfarmer.dev/wp-content/uploads/2016/09/light-bulb.png" alt="Light Bulb Icon" width="200" height="200" /><br>
+				<img class="icon" src="http://www.evanjfarmer.dev/wp-content/uploads/2016/10/light-bulb.png" alt="Light Bulb Icon" width="120" height="120" /><br>
 				<strong><p>idea creation & implementation</p></strong></div>
 				<div class="small-6 large-3 columns">
-				<img class="icon" src="http://www.evanjfarmer.dev/wp-content/uploads/2016/09/like.png" alt="Like Icon" width="200" height="200" /><br>
+				<img class="icon" src="http://www.evanjfarmer.dev/wp-content/uploads/2016/10/like.png" alt="Like Icon" width="120" height="120" /><br>
 				<strong><p>social strategy & branding</p></strong></div>
 				<div class="small-6 large-3 columns">
-				<img class="basket-icon" src="http://www.evanjfarmer.dev/wp-content/uploads/2016/09/basket.png" alt="Basket Icon" width="200" height="200" />
+				<img class="basket-icon" src="http://www.evanjfarmer.dev/wp-content/uploads/2016/10/basket.png" alt="Basket Icon" width="120" height="120" />
 				<strong><p>ecommerce solutions</p></strong></div>
 				&nbsp;
 				&nbsp;
@@ -68,18 +68,17 @@
 		  		<div class="learn small-12 medium-4 large-4 text-center columns">
 		  			<img src="http://www.evanjfarmer.dev/wp-content/uploads/2016/09/chart.png" alt="Chart Icon" width="150" height="150" /><br>
 		  			<br><h4><strong>More than 25% of the internet is powered by WordPress, and that number is growing!</strong></h4><h4><strong>Find out why it's the best...<strong></h4>
-		  			<br><a href="/webdev-with-wordpress/" class="button">LEARN MORE</a><br><br>
-		  			<br>
+		  			<br><a href="/webdev-with-wordpress/" class="button">LEARN MORE</a><hr>
 		  		</div>
 				<div class="learn small-12 medium-4 large-4 text-center columns">
 					<img src="http://www.evanjfarmer.dev/wp-content/uploads/2016/09/tablet.png" alt="Tablet Icon" width="150" height="150" /></br>
 					<br><h4><strong>Responsive design, easy to implement for content managment, and lots of plug-ins and add-ons to help you do just about anything.</strong></h4>				
-					<br><a href="/responsiveness/" class="button">LEARN MORE</a><br><br>
+					<br><a href="/responsiveness/" class="button">LEARN MORE</a><hr>
 		  		</div>
 		  		<div class="learn small-12 medium-4 large-4 text-center columns">
 		  			<img src="http://www.evanjfarmer.dev/wp-content/uploads/2016/09/magnifier.png" alt="Magnifier Icon"	width="150" height="150" /><br>
 		  			<br><h4><strong>Search engine friendly, reliable, safe and secure, extensible, simple to use, and <a href="https://wordpress.org/showcase/">trusted by many</a>!</h4><h4><strong>You will love it too.</strong></h4>
-		  			<br><a href="/value-of-a-website/" class="button">LEARN MORE</a><br><br>
+		  			<br><a href="/value-of-a-website/" class="button">LEARN MORE</a><hr>
 				</div>
 			</div>
 		</div>
