8628406c403eb01f758d6e2f193e7c0b645bfd9b
evanjfarmer <evanjfarmer@gmail.com>
2016-11-21 21:53:56 -0500

    Added <hr> tags to break up projects page

--- template-parts/content-page-projects.php
+++ template-parts/content-page-projects.php
@@ -23,16 +23,14 @@
   		<div class="red-cedar small-12 large-4 columns">
         &nbsp;
   			<a href="http://www.redcedarbirth.com"><h3>Red Cedar Birth</h3></a>
-  			<a href="http://www.redcedarbirth.com"><img class="red-cedar" src="http://www.evanjfarmer.com/wp-content/uploads/2016/10/red-cedar-site.jpg" alt="Red Cedar Site" width="500" height="400"/></a>
+  			<a href="http://www.redcedarbirth.com"><img class="red-cedar" src="http://www.evanjfarmer.com/wp-content/uploads/2016/10/red-cedar-site.jpg" alt="Red Cedar Site" width="500" height="400"/></a><hr>
   		</div>
-		<div class="red-cedar-info small-12 large-4 columns">
-        &nbsp;
-  			<h4>The Red Cedar Birth website was developed to serve as a resource for expectant mothers. RCB offers midwifery and doula services, and they sell herbal personal care products.</h4><br><h4> This site features an ecommerce web store, a community resource page, and a Google-integrated event calendar.</h4><a href="http://www.redcedarbirth.com"><h4 class="check-it"><< Check it out! >></h4></a>
-  			<h5></h5> 
+		  <div class="red-cedar-info small-12 large-4 columns">
+  			<h4>The Red Cedar Birth website was developed to serve as a resource for expectant mothers. RCB offers midwifery and doula services, and they sell herbal personal care products.</h4><br><hr><br><h4> This site features an ecommerce web store, a community resource page, and a Google-integrated event calendar.</h4><a href="http://www.redcedarbirth.com"><h4 class="check-it"><< Check it out! >></h4></a> 
   		</div>
       <div class="red-cedar-store small-12 large-4 columns">
         &nbsp;
-        <a href="http://www.redcedarbirth.com"><img class="red-cedar" src="http://www.evanjfarmer.com/wp-content/uploads/2016/10/red-cedar-store.jpg" alt="Red Cedar Store" width="500" height="400"/></a>
+        <a href="http://www.redcedarbirth.com"><img class="red-cedar" src="http://www.evanjfarmer.com/wp-content/uploads/2016/10/red-cedar-store.jpg" alt="Red Cedar Store" width="500" height="400"/></a><hr>
       </div>
   	</div>
     <div class="red-cedar-origin row">
@@ -44,10 +42,10 @@
       <div class="webstore-mobile large-3 columns"><h2></h2>
       </div>
     </div>
-       <div class="more-soon">
-          &nbsp;
-          <h3>More portfolio pieces and project features coming soon!</h3>
-       </div>
+    <div class="more-soon">
+      &nbsp;
+      <h3>More portfolio pieces and project features coming soon!</h3>
+    </div>
     <hr>
     <h1 class="projects-title">Other Endeavors</h1>
     <div class="row">
@@ -55,15 +53,18 @@
         &nbsp;
         <a href="http://www.meetup.com/WordPress-Ann-Arbor/"><h3>A2 WP Meetup</h3></a>
         <a href="http://www.meetup.com/WordPress-Ann-Arbor/"><img class="wp-meetup" src="http://www.evanjfarmer.com/wp-content/uploads/2016/11/A2WP.jpg" alt="A2 WP Meetup" width="500" height="350"/></a>
+        <hr>
       </div>
       <div class="small-12 large-4 columns">
         <a href="https://www.meetup.com/topics/wordpress/"><img class="wp-meetup" src="http://www.evanjfarmer.com/wp-content/uploads/2016/10/wordpress-4-240.png" alt="WP Meetup" width="240" height="240"/></a>
+        <hr>
         <h4>I'm involved in both the Ann Arbor and Jackson Michigan WordPress Meetup groups and would love to connect with others seeking to learn and grow.</h4>
       </div>
       &nbsp;
       <div class="small-12 large-4 columns">
         <a href="http://www.meetup.com/JXNWP-meetup/"><h3>JXN WP Meetup</h3></a>
         <a href="http://www.meetup.com/JXNWP-meetup/"><img class="wp-meetup" src="http://www.evanjfarmer.com/wp-content/uploads/2016/11/JXNWP.png" alt="Jackson WP Meetup" width="500" height="350"/></a>
+        <hr>
       </div>
     </div> 
     &nbsp; 
@@ -73,7 +74,7 @@
           <a href="https://2015.annarbor.wordcamp.org/"><img class="wc2015" src="http://www.evanjfarmer.com/wp-content/uploads/2016/11/WC2015.jpg"></a>
           &nbsp;
           <h4>I've helped organize, promote, and put together WordCamp Ann Arbor for the last couple of years...</h4>
-          <br>
+          <hr><br>
           <a href="https://2016.annarbor.wordcamp.org/"><img class="wc2016" src="http://www.evanjfarmer.com/wp-content/uploads/2016/11/WC2016.jpg"></a>
           &nbsp;
           <h4>...and I'm on the planning team for the first ever <a href="https://2017.jackson.wordcamp.org/"><strong>WordCamp JXN</strong></a> in 2017, which I'll also be speaking at!</h4>
